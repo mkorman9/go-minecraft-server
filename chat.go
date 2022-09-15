571066857541024238088260353dc88de92df041
Michał Korman <michal.korman@icloud.com>
2022-09-16 01:03:36 +0200

    better api for building chat messages

--- chat.go
+++ chat.go
@@ -3,14 +3,14 @@ package main
 import "encoding/json"
 
 type ChatMessage struct {
-	Text          string        `json:"text"`
-	Bold          bool          `json:"bold,omitempty"`
-	Italic        bool          `json:"italic,omitempty"`
-	Underlined    bool          `json:"underlined,omitempty"`
-	Strikethrough bool          `json:"strikethrough,omitempty"`
-	Obfuscated    bool          `json:"obfuscated,omitempty"`
-	Font          string        `json:"font,omitempty"`
-	Extra         []ChatMessage `json:"extra,omitempty"`
+	Text            string        `json:"text"`
+	IsBold          bool          `json:"bold,omitempty"`
+	IsItalic        bool          `json:"italic,omitempty"`
+	IsUnderlined    bool          `json:"underlined,omitempty"`
+	IsStrikethrough bool          `json:"strikethrough,omitempty"`
+	IsObfuscated    bool          `json:"obfuscated,omitempty"`
+	FontName        string        `json:"font,omitempty"`
+	Extra           []ChatMessage `json:"extra,omitempty"`
 }
 
 const (
@@ -19,9 +19,39 @@ const (
 
 func NewChatMessage(text string) *ChatMessage {
 	return &ChatMessage{
-		Text: text,
-		Font: FontDefault,
+		Text:     text,
+		FontName: FontDefault,
 	}
+}
+
+func (cm *ChatMessage) Bold() *ChatMessage {
+	cm.IsBold = true
+	return cm
+}
+
+func (cm *ChatMessage) Italic() *ChatMessage {
+	cm.IsItalic = true
+	return cm
+}
+
+func (cm *ChatMessage) Underlined() *ChatMessage {
+	cm.IsUnderlined = true
+	return cm
+}
+
+func (cm *ChatMessage) Strikethrough() *ChatMessage {
+	cm.IsStrikethrough = true
+	return cm
+}
+
+func (cm *ChatMessage) Obfuscated() *ChatMessage {
+	cm.IsObfuscated = true
+	return cm
+}
+
+func (cm *ChatMessage) Font(font string) *ChatMessage {
+	cm.FontName = font
+	return cm
 }
 
 func (cm *ChatMessage) Encode() string {
